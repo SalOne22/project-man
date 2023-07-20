8d0a0f01d88d07eb955def3e5749123be94ce886
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-07-20 12:26:20 +0200

    Added Layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+      </Routes>
+    </>
+  );
 };
