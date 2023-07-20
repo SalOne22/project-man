a7ed4a34f0aeec431cd58109534684494cd46abe
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-07-20 14:35:55 +0200

    Added Header element

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,52 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useTransition, animated } from '@react-spring/web';
+import { Search } from 'lucide-react';
+import { ReactComponent as Logo } from 'assets/logo.svg';
+
 export const Header = () => {
-  return <div>Header</div>;
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const searchTransition = useTransition(isSearchOpen, {
+    from: {
+      y: -50,
+      opacity: 0,
+    },
+    enter: {
+      y: 0,
+      opacity: 1,
+    },
+    leave: {
+      y: -50,
+      opacity: 0,
+    },
+  });
+
+  return (
+    <header className="navbar bg-base-200">
+      <Link to="/" className="btn btn-ghost normal-case text-xl">
+        <Logo width="1em" height="1em" />
+        <h1>Project Man</h1>
+      </Link>
+
+      <div className="ml-auto">
+        {searchTransition(
+          (style, item) =>
+            item && (
+              <animated.input
+                style={style}
+                placeholder="Search..."
+                className="input input-bordered input-sm mr-4
+                absolute right-0 top-20 sm:top-0 sm:relative"
+              />
+            )
+        )}
+        <button
+          onClick={() => setIsSearchOpen((prev) => !prev)}
+          className="btn btn-ghost btn-circle"
+        >
+          <Search />
+        </button>
+      </div>
+    </header>
+  );
 };
