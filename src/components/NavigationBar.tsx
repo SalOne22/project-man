5a39f11d1fe7a622e2bb301be51bb35d74b4023a
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-07-20 15:30:16 +0200

    Updated navigation bar appearance

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -10,42 +10,66 @@ import {
 
 export const NavigationBar = () => {
   return (
-    <aside className="drawer-side">
+    <aside className="drawer-side overflow-visible">
       <label htmlFor="navigation" className="drawer-overlay"></label>
       <nav className="p-1 h-full bg-base-300">
         <ul className="menu gap-1">
           <li>
-            <NavLink to="/">
+            <NavLink
+              to="/"
+              className="sm:tooltip sm:tooltip-right"
+              data-tip="Home"
+            >
               <Home />
               <span className="sm:hidden">Home</span>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/projects">
+            <NavLink
+              to="/projects"
+              className="sm:tooltip sm:tooltip-right"
+              data-tip="Projects"
+            >
               <FolderOpen />
               <span className="sm:hidden">Projects</span>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/tasks">
+            <NavLink
+              to="/tasks"
+              className="sm:tooltip sm:tooltip-right"
+              data-tip="Tasks"
+            >
               <CheckSquare />
               <span className="sm:hidden">Tasks</span>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/calendar">
+            <NavLink
+              to="/calendar"
+              className="sm:tooltip sm:tooltip-right"
+              data-tip="Calendar"
+            >
               <CalendarClock />
               <span className="sm:hidden">Calendar</span>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/reports">
+            <NavLink
+              to="/reports"
+              className="sm:tooltip sm:tooltip-right"
+              data-tip="Reports"
+            >
               <Flag />
               <span className="sm:hidden">Reports</span>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/settings">
+            <NavLink
+              to="/settings"
+              className="sm:tooltip sm:tooltip-right"
+              data-tip="Settings"
+            >
               <Settings />
               <span className="sm:hidden">Settings</span>
             </NavLink>
