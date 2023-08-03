fd92849edc64f8c66f7e4a638e33e4665a2ab3df
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-03 18:51:06 +0200

    Added loader

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,22 @@
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
-import { NotFound } from '../pages/NotFound';
-import { Home } from 'pages/Home';
+import { FullLoader } from './FullLoader';
+
+const Home = lazy(() => import('pages/Home'));
+const NotFound = lazy(() => import('pages/NotFound'));
 
 export const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<FullLoader />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </>
   );
 };
