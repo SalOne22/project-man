09316e0974d3d3575ebf63a0723de45999117745
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-06 12:49:10 +0200

    Fixed lint issues

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
 import './index.css';
 
+//? div with id root always in index.html
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter basename="project-man">
